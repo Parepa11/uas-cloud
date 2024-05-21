5a7a544050fba0e5f171aed88ebd085e08a048d7
Parepa11 <megumiparepatimbang@gmail.com>
2024-05-21 07:59:35 +0700

    Add files via upload

---
+++ uas cloud.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>uas cloud computing</title>
+    <link rel="stylesheet" href="style5.css">
+</head>
+<body>
+    <header>
+        <li>foto profilku</li>
+    </header>
+    <div class="container">
+   <nav>
+       <div class="">
+           <img src="" alt="">
+       </div>
+       <div class="menu">
+           <ul>
+           </ul>
+       </div>
+   </nav>     
+   
+<div class="jumbotron">
+    <h3>yeyen megumi parepa </h3>
+</div>
+<div class="info">
+<div class="foto umur 3 tahun">
+    <h3>foto kecil</h3>
+    <hr size="3" color="red">
+    <img src="foto kecil.jpeg" alt=""> 
+  
+    <p>Senin, 28 Maret 2022 | sedikit cerita </p>
+    <p>
+      nama lengkap:yeyen megumi parepa
+      umur di foto:3 tahun
+      alamat: rante pasele
+    </p>
+</div>
+<div class="berita">
+    <h3>foto di acar rambu solo</h3>
+    <hr size="3" color="red">
+    <img src="fotosekarang.jpg" alt="">
+    <p>
+        foto saat dimana acara keluarga yaitu rambu solo
+    </p>
+</div>
+</div>
+   
+    
+               
+
+    </div>
+</body>
+</html>
